2b50e003136307da8648c57828ed21751b058530
Giovani Gambero <nanigambero@gmail.com>
2023-07-04 10:57:52 -0300

    new_version: node server added to fix cors issue

---
+++ demo/demo.js
@@ -0,0 +1,32 @@
+const options = {
+    environment: 'prod',
+    permissionButton: {
+      label: 'Permitir',
+      color: '#F9F9F9',
+      backgroundColor: '#39C560',
+      borderRadius: '10px',
+      border: '1px solid'
+    },
+    startButton: {
+      label: 'Escanear rosto',
+      color: '#F9F9F9',
+      backgroundColor: 'blue',
+      borderRadius: '0.25rem',
+      border: '1px solid #2D994B'
+    }
+  } 
+  
+  const token = "";
+  const useFaceAuthenticator = false;
+  const sdkContainer = 'sdk-container';
+  const personId = ''
+  async function initSdk() {
+    const sdk = window['FacesSDK'];
+    const facesSdk = await sdk.initializeSdk(token, sdkContainer, useFaceAuthenticator, personId, options);
+    const result = await facesSdk.execute();
+    console.log(result)
+  }
+  
+  setTimeout(() => {
+    initSdk();
+  }, 5000)
---
+++ server.js
@@ -0,0 +1,31 @@
+const Hapi = require("@hapi/hapi")
+const Inert = require("@hapi/inert")
+const resolve = require("path").resolve
+
+console.log(__dirname)
+async function init() {
+    const ALL_INTERFACES = "0.0.0.0"
+    const PORT = 3000
+    const server = Hapi.server({
+      port: PORT,
+      host: ALL_INTERFACES,
+    })
+  
+    await server.register(Inert)
+  
+    server.route({
+      method: "GET",
+      path: "/{param*}",
+      options: { cors: { origin: ["*"] } },
+      handler: {
+        directory: {
+          path: resolve(__dirname, "demo"),
+          index: "index.html",
+        },
+      },
+    })
+
+    await server.start()
+}
+
+init();
